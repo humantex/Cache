3a2d03649ef4dc636fcee4bcbb76e1552497cda7
humantex <humantex@users.noreply.github.com>
2020-08-20 23:58:12 +0000

    Apply fixes from StyleCI

--- CacheInterface.php
+++ CacheInterface.php
@@ -1,131 +1,146 @@
-<?php namespace Gregwar\Cache;
+<?php
 
-interface CacheInterface {
+namespace Gregwar\Cache;
 
+interface CacheInterface
+{
     /**
-     * Sets the cache directory
+     * Sets the cache directory.
      *
      * @param string $cacheDirectory the cache directory
+     *
      * @return self
      */
     public function setCacheDirectory($cacheDirectory);
 
     /**
-     * Gets the cache directory
+     * Gets the cache directory.
      *
      * @return string the cache directory
      */
     public function getCacheDirectory();
 
     /**
-     * Sets the actual cache directory
+     * Sets the actual cache directory.
      *
      * @param string $actualCacheDirectory the actual cache directory
+     *
      * @return self
      */
     public function setActualCacheDirectory($actualCacheDirectory = null);
 
     /**
-     * Returns the actual cache directory
+     * Returns the actual cache directory.
      */
     public function getActualCacheDirectory();
 
     /**
-     * Change the prefix size
+     * Change the prefix size.
      *
      * @param int $prefixSize the size of the prefix directories
+     *
      * @return self
      */
     public function setPrefixSize($prefixSize);
 
     /**
-     * Change the directory mode
+     * Change the directory mode.
      *
      * @param int $directoryMode the directory mode to use
+     *
      * @return self
      */
     public function setDirectoryMode($directoryMode);
 
     /**
-     * Gets the cache file name
+     * Gets the cache file name.
      *
      * @param string $filename the name of the cache file
-     * @param bool $actual get the actual file or the public file
-     * @param bool $mkdir a boolean to enable/disable the construction of the
-     *        cache file directory
+     * @param bool   $actual   get the actual file or the public file
+     * @param bool   $mkdir    a boolean to enable/disable the construction of the
+     *                         cache file directory
+     *
      * @return string
      */
     public function getCacheFile($filename, $actual = false, $mkdir = false);
 
     /**
      * Checks if the target filename exists in the cache and if the conditions
-     * are respected
+     * are respected.
      *
-     * @param string $filename the filename
-     * @param array $conditions the conditions to respect
+     * @param string $filename   the filename
+     * @param array  $conditions the conditions to respect
+     *
      * @return bool
      */
-    public function exists($filename, array $conditions = array());
+    public function exists($filename, array $conditions = []);
 
     /**
-     * Alias for exists
+     * Alias for exists.
      *
-     * @param string $filename the filename
-     * @param array $conditions the conditions to respect
+     * @param string $filename   the filename
+     * @param array  $conditions the conditions to respect
+     *
      * @return bool
      */
-    public function check($filename, array $conditions = array());
+    public function check($filename, array $conditions = []);
 
     /**
-     * Write data in the cache
+     * Write data in the cache.
      *
      * @param string $filename the name of the cache file
      * @param string $contents the contents to store
+     *
      * @return self
      */
     public function set($filename, $contents = '');
 
     /**
-     * Alias for set()
+     * Alias for set().
      *
      * @param string $filename the name of the cache file
      * @param string $contents the contents to store
+     *
      * @return self
      */
     public function write($filename, $contents = '');
 
     /**
-     * Get data from the cache
+     * Get data from the cache.
      *
-     * @param string $filename the cache file name
-     * @param array $conditions
+     * @param string $filename   the cache file name
+     * @param array  $conditions
+     *
      * @return null|string
      */
-    public function get($filename, array $conditions = array());
+    public function get($filename, array $conditions = []);
 
     /**
-     * Get or create the cache entry
+     * Get or create the cache entry.
      *
-     * @param string $filename the cache file name
-     * @param array $conditions an array of conditions about expiration
-     * @param \Closure $function the closure to call if the file does not exist
-     * @param bool $file returns the cache file or the file contents
-     * @param bool $actual returns the actual cache file
-     * @return string
+     * @param string   $filename   the cache file name
+     * @param array    $conditions an array of conditions about expiration
+     * @param \Closure $function   the closure to call if the file does not exist
+     * @param bool     $file       returns the cache file or the file contents
+     * @param bool     $actual     returns the actual cache file
+     *
      * @throws \InvalidArgumentException
+     *
+     * @return string
      */
-    public function getOrCreate($filename, array $conditions = array(), $function, $file = false, $actual = false);
+    public function getOrCreate($filename, array $conditions = [], $function, $file = false, $actual = false);
 
     /**
-     * Alias to getOrCreate with $file = true
+     * Alias to getOrCreate with $file = true.
      *
-     * @param string $filename the cache file name
-     * @param array $conditions an array of conditions about expiration
-     * @param \Closure $function the closure to call if the file does not exist
-     * @param bool $actual returns the actual cache file
-     * @return string
+     * @param string   $filename   the cache file name
+     * @param array    $conditions an array of conditions about expiration
+     * @param \Closure $function   the closure to call if the file does not exist
+     * @param bool     $actual     returns the actual cache file
+     *
      * @throws \InvalidArgumentException
+     *
+     * @return string
      */
-    public function getOrCreateFile($filename, array $conditions = array(), $function, $actual = false);
-
+    public function getOrCreateFile($filename, array $conditions = [], $function, $actual = false);
 }
--- demo/red.php
+++ demo/red.php
@@ -1,15 +1,15 @@
 <?php
 
-include('../autoload.php'); // If using composer
+include '../autoload.php'; // If using composer
 
 use Gregwar\Cache\Cache;
 
-$cache = new Cache;
+$cache = new Cache();
 $cache->setCacheDirectory('cache'); // This is the default
 
 // If the cache exists, this will return it, else, the closure will be called
 // to create this image
-$data = $cache->getOrCreate('red-square.png', array(), function($filename) {
+$data = $cache->getOrCreate('red-square.png', [], function ($filename) {
     $i = imagecreatetruecolor(100, 100);
     imagefill($i, 0, 0, 0xff0000);
     file_put_contents($filename, 'abc');
